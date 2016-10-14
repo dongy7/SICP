a068f14aa0a69242023761ec4982cc81727fa351
dongy7 <dongy7@gmail.com>
2016-10-14 18:42:09 -0400

    add solution to 1.7

---
+++ chapter1/1.07.scm
@@ -0,0 +1,21 @@
+(define (good-enough? guess prev)
+  (< (abs (- guess prev)) 0.000001))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 0.0 x))
+
+(define (sqrt-iter guess prev x)
+  (if (good-enough? guess prev)
+      guess
+      (sqrt-iter (improve guess x) guess x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y) 
+  (/ (+ x y) 2))
+
+(sqrt 9)
+(sqrt (+ 100 37))
+(sqrt (+ (sqrt 2) (sqrt 3)))
+(square (sqrt 1000))
