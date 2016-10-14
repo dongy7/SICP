5254221ff5c4b60440dbce57821d72f09d8b0af5
dongy7 <dongy7@gmail.com>
2016-10-14 15:17:19 -0400

    convert tabs to spaces and add 1.5

--- chapter1/1.03.scm
+++ chapter1/1.03.scm
@@ -1,15 +1,15 @@
 (define (>= x y)
-	(or (> x y) (= x y)))
+  (or (> x y) (= x y)))
 
 (define (square x) (* x x))
 
 (define (sum-of-squares x y) 
-	(+ 
-		(square x) 
-		(square y)))
+  (+ 
+    (square x) 
+    (square y)))
 
 (define (f x y z)
-	(cond ((and (>= x y) (>= y z)) (sum-of-squares x y))
-				((and (>= x y) (>= z y)) (sum-of-squares x z))
-				((and (>= y x) (>= z x)) (sum-of-squares y z))
-				((and (= x y) (= x z)) (sum-of-squares x y))))
+  (cond ((and (>= x y) (>= y z)) (sum-of-squares x y))
+        ((and (>= x y) (>= z y)) (sum-of-squares x z))
+        ((and (>= y x) (>= z x)) (sum-of-squares y z))
+        ((and (= x y) (= x z)) (sum-of-squares x y))))
--- chapter1/1.05.scm
+++ chapter1/1.05.scm
@@ -8,5 +8,15 @@
 (test 0 (p))
 
 ; If appicative-order evaluation is used, 
-; then the expression would return 0, since
+; then the expression would not return
+; because the interpreter would try to evaluate
+; (p), which just evaluates to (p) and never terminates.
 ; 
+; If normal-order evaluation is used,
+; then the expression would return 0,
+; because it would never evaluate (p) since it doesn't need to.
+;
+; (test 0 (p))
+; (if (= 0 0) 0 (p))
+; (if #t 0 (p))
+; 0
