daef72c1dff52ba653ff1300f81198e9e3e4a0cd
dongy7 <dongy7@gmail.com>
2016-10-14 18:18:34 -0400

    add 1.6 solution

---
+++ chapter1/1.06.scm
@@ -0,0 +1,14 @@
+(define (new-if predicate 
+                then-clause 
+                else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x) x)))
+
+; Using the new-if will result in infinite recurion
+; since it will use applicative order evaluation and
+; will try to evaluate the operands. 
