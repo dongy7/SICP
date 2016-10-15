9ebdb21cf62c61f2c1321b0f89e265229b8c3343
dongy7 <dongy7@gmail.com>
2016-10-14 22:29:32 -0400

    fix sum of highest squares

--- src/chapter1/1.03.rkt
+++ src/chapter1/1.03.rkt
@@ -1,19 +1,15 @@
 #lang racket/base
 
-(define (>= x y)
-  (or (> x y) (= x y)))
+(define (max x y)
+  (if (> x y) x y))
+
+(define (min x y)
+  (if (< x y) x y))
 
 (define (square x) (* x x))
 
-(define (sum-of-squares x y) 
-  (+ 
-    (square x) 
-    (square y)))
+(define (sum-of-highest-squares x y z)
+  (+ (square(max x y))
+     (square (max (min x y) z))))
 
-(define (f x y z)
-  (cond ((and (>= x y) (>= y z)) (sum-of-squares x y))
-        ((and (>= x y) (>= z y)) (sum-of-squares x z))
-        ((and (>= y x) (>= z x)) (sum-of-squares y z))
-        ((and (= x y) (= x z)) (sum-of-squares x y))))
-
-(provide f)
+(provide sum-of-highest-squares)
--- test/1.03-test.rkt
+++ test/1.03-test.rkt
@@ -3,10 +3,10 @@
 (require rackunit
          "../src/chapter1/1.03.rkt")
 
-(check-equal? (f 1 1 1) 2  "Simple sum")
-(check-equal? (f 1 2 3) 13 "Sum from three unique elements")
-(check-equal? (f 1 3 2) 13 "Sum from three unique elements")
-(check-equal? (f 2 1 3) 13 "Sum from three unique elements")
-(check-equal? (f 2 3 1) 13 "Sum from three unique elements")
-(check-equal? (f 3 2 1) 13 "Sum from three unique elements")
-(check-equal? (f 3 2 1) 13 "Sum from three unique elements")
+(check-equal? (sum-of-highest-squares 1 1 1) 2  "Simple sum")
+(check-equal? (sum-of-highest-squares 1 2 3) 13 "Sum from three unique elements")
+(check-equal? (sum-of-highest-squares 1 3 2) 13 "Sum from three unique elements")
+(check-equal? (sum-of-highest-squares 2 1 3) 13 "Sum from three unique elements")
+(check-equal? (sum-of-highest-squares 2 3 1) 13 "Sum from three unique elements")
+(check-equal? (sum-of-highest-squares 3 2 1) 13 "Sum from three unique elements")
+(check-equal? (sum-of-highest-squares 3 2 1) 13 "Sum from three unique elements")
